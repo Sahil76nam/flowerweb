db3750fae085f1a9c51944230d05c1bd49b9128c
Sahil <namdevsahil6@gamil.com>
2025-01-17 15:32:26 +0530

    moving

---
+++ main.js
@@ -0,0 +1,34 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('main'),
+    smooth: true
+});
+
+
+
+let getdata=async()=>{
+    let url='http://localhost:3000/flowerdeliver ';
+    let res=await fetch(url,{method:"GET"})
+    let data1=await res.json()
+
+let show=document.querySelector("#display")
+data1.map((e)=>{
+    show.innerHTML+=`
+    <div class="data">
+    <h1>${e.name}</h1>
+    <h1>${e.number}</h1>
+    <h1>${e.email}</h1>
+    <h1>${e.payment}</h1>
+    <h1>${e.city}</h1>
+    <h1>${e.state}</h1>
+    <h1>${e.pincode}</h1>
+    </div>`
+})}
+
+var elem=document.querySelectorAll(".elem")
+var page1=document.querySelector("#page1")
+elem.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+       var bgimg= e.getAttribute("data-image")
+       page1.style.backgroundImage=`url(${bgimg})`
+    })
+})
