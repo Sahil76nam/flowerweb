c5fe303bd7536b40d5e7ee64192addc5961d8a90
Sahil <namdevsahil6@gamil.com>
2025-01-20 19:31:25 +0530

    json

--- order.js
+++ order.js
@@ -17,7 +17,7 @@ data1.map((e)=>{
     <h1>${e.pincode}</h1>
     <h1>${e.orderstatus}</h1>
     <h1 onclick="del('${e.id}')">DELETE</h1>
-    <h1 onclick="update('${e.id}')">UPDATE</h1>
+    <h1 onclick="updatee('${e.id}')">UPDATE</h1>
     </div>`
 })}
 
@@ -58,3 +58,58 @@ let ins=()=>{
     location.href="deliver.html"
     return false;
 }
+
+let updatee=async(id)=>{
+   
+    let url=`http://localhost:3000/flowerdeliver/${id}`
+    let res=await fetch(url)
+    let data= await res.json()
+    
+
+    let formdata=` enter name: <input type="text" id="dataname" value=${data.name}>
+        enter NUMBER: <input type="number" id="datanumber"value=${data.number}>
+        enter email: <input type="text"  id="dataemail"value=${data.email}>
+        PAYMENT mode: <input type="text"  id="datapayment"value=${data.payment}>
+        enter city: <input type="text"  id="datacity" value=${data.city}>
+        enter state: <input type="text"  id="datastate" value=${data.state}>
+        PAYMENT pincode: <input type="text"  id="datapincode" value=${data.pincode}>
+        STATUS <input type="text"  id="datastatus" value=${data.status}>
+        <button type="submit" value="Update" onclick=" return finalupdate(${data.id})">update</button>
+`
+
+document.querySelector("#updateform").innerHTML=formdata
+}
+
+let finalupdate=(id)=>{
+    let dataname=document.querySelector("#dataform").value
+    let datanumber=document.querySelector("#datanumber").value
+    let dataemail=document.querySelector("#dataemail").value
+    let datapayment=document.querySelector("#datapayment").value
+    let datacity=document.querySelector("#datacity").value
+    let datastate=document.querySelector("#datastate").value
+    let datapincode=document.querySelector("#datapincode").value
+    let datastatus=document.querySelector("#datastatus").value
+
+
+    let url=`http://localhost:3000/flowerdeliver/${id}`
+
+    fetch(url,{
+        method:"PUT",
+        headers:{
+            "Content_Type":"application/json"
+        },
+        body:JSON.stringify({
+            name:dataname,
+            number:datanumber,
+            email:dataemail,
+            payment:datapayment,
+            city:datacity,
+            state:datastate,
+            pincode:datapincode,
+            status:datastatus
+
+        })
+    })
+    return false
+}
+
